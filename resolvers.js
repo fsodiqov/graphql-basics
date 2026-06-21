646608458aee30e4a23d25e9bd7bfa18f7db3232
fsodiqov <foziljonsodiq@gmail.com>
2026-06-21 22:05:21 +0500

    feat: add single-item queries for game, review, and author

--- resolvers.js
+++ resolvers.js
@@ -3,7 +3,10 @@ import { games, reviews, authors } from './_db.js'
 export const resolvers = {
   Query: {
     games: () => games,
-    reviews: () => reviews,
+    game: (_parent, args) => games.find(game => game.id === args.id),
+    reviews: () => reviews, 
+    review: (_parent, args) => reviews.find(review => review.id === args.id),
     authors: () => authors,
+    author: (_parent, args) => authors.find(author => author.id === args.id),
   },
 }
